407fc7e06c0f07d9fe5b196cada7ac92bcb68de6
Sally O'Malley <somalley@redhat.com>
2018-08-03 23:48:38 -0400

    test

--- hack/gotest.sh
+++ hack/gotest.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-# in prow, already in container, so no 'docker run'
 if [ "$IS_CONTAINER" != "" ]; then
-  echo path
-  echo ${PATH}
-  CGO_ENABLED=0 go test -v -cover $(go list ./installer/...)
+  cd ..
+  SOURCE_DIR="$(pwd)"
+  cd .. || exit
+  ROOT_DIR="$(pwd)"
+  # if running in a local prow instance, with repo ../not-openshift/installer, have to modify here
+  TARGET_DIR="${ROOT_DIR}/openshift"
+  if [ "$SOURCE_DIR" != "$TARGET_DIR" ]; then
+    mv "$SOURCE_DIR" "$TARGET_DIR"
+  fi;
+  cd "${TARGET_DIR}/installer/" || exit
+  CGO_ENABLED=0 go test ./installer/...
 else
-    docker run -e IS_CONTAINER='TRUE' -v "$(pwd)":/usr/local/go/src/github.com/openshift/installer  -w /usr/local/go/src/github.com/openshift/installer quay.io/coreos/golang-testing ./hack/gotest.sh
+  docker run -e IS_CONTAINER='TRUE' -v "$PWD":/go/src/github.com/openshift/installer -w /go/src/github.com/openshift/installer quay.io/coreos/golang-testing ./hack/gotest.sh
 fi
-
